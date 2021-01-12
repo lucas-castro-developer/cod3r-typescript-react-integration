96f266d6cc9fbb099110284dddc249a7720b46cf
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-12 18:58:03 -0300

    Funcional components, applying types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import ContadorValor from './components/ContadorValor'
 
 function App() {
   return (
     <div className="App">
+      <ContadorValor contador={111} />
     </div>
   );
 }
---
+++ src/components/ContadorValor.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface ContadorValorProps{
+    contador: number
+}
+
+export default (props: ContadorValorProps) => {
+    return (<p>{props.contador}</p>)
+}
