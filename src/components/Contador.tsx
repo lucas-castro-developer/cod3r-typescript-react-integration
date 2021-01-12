cb7e018e3f555e2d0405b1881e8aa73e5207d22f
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-12 19:23:24 -0300

    Class components: applying types #02

--- src/components/Contador.tsx
+++ src/components/Contador.tsx
@@ -11,9 +11,20 @@ interface IContadorState {
 
 export default class Contador extends Component<IContadorProps, IContadorState> {
     public state = { valor: this.props.valorInicial || 0 }
+    
+    private setValor = (delta: number) => {
+        this.setState({
+            valor: this.state.valor + delta
+        })
+    }
+    
     render() {
         return (
-            <ContadorValor contador={this.state.valor} />
+            <div>
+                <ContadorValor contador={this.state.valor} />
+                <button onClick={() => this.setValor(10)}>+</button>
+                <button onClick={() => this.setValor(-10)}>-</button>
+            </div>
         )
     }
 }
